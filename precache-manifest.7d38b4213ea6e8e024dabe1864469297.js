11d5e32fc47f9ea7b3e226e15b11207aaff204c7
ask164 <akshaysk164@gmail.com>
2020-05-03 22:23:37 +0530

    Updates

---
+++ precache-manifest.7d38b4213ea6e8e024dabe1864469297.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae7600ad31485a496ca82b566c58aaf2",
+    "url": "/covid-19-reports/index.html"
+  },
+  {
+    "revision": "86bd50afb7589a0afa09",
+    "url": "/covid-19-reports/static/css/2.228ab239.chunk.css"
+  },
+  {
+    "revision": "8570bb964a5a7a5d8e6a",
+    "url": "/covid-19-reports/static/css/main.ce913660.chunk.css"
+  },
+  {
+    "revision": "86bd50afb7589a0afa09",
+    "url": "/covid-19-reports/static/js/2.b2bb3e89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-reports/static/js/2.b2bb3e89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8570bb964a5a7a5d8e6a",
+    "url": "/covid-19-reports/static/js/main.159bd47f.chunk.js"
+  },
+  {
+    "revision": "1b42404817dd9b185400",
+    "url": "/covid-19-reports/static/js/runtime-main.bb178eb6.js"
+  }
+]);
